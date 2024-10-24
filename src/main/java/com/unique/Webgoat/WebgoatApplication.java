65f7be2ff45234985eaec1cefebbfb26552647ee
ualteamleader <ualteamleader@gmail.com>
2024-10-24 00:38:16 -0700

    First Commit from Webgoat

---
+++ src/main/java/com/unique/Webgoat/WebgoatApplication.java
@@ -0,0 +1,13 @@
+package com.unique.Webgoat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgoatApplication.class, args);
+	}
+
+}
